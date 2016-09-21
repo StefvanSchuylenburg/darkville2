885df37d6fb8d91a65173dd7c65f9e2939a62f57
Stef van Schuylenburg <stef.vanschuylenburg@trifork.nl>
2016-09-21 23:59:25 +0200

    Add login security This includes some dummy account/password combinations

---
+++ web/src/main/kotlin/me/darkville/web/WebSecurityConfig.kt
@@ -0,0 +1,29 @@
+package me.darkville.web
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+/**
+ * The configuration for the security.
+ * For now this is using a fixed set of accounts/passwords.
+ */
+@Configuration
+@EnableWebSecurity
+open class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .antMatchers("/games/**").authenticated()
+                .and().formLogin()
+    }
+
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication().withUser("wolfje1").password("wachtwoord1").roles()
+        auth.inMemoryAuthentication().withUser("wolfje2").password("wachtwoord2").roles()
+        auth.inMemoryAuthentication().withUser("wolfje3").password("wachtwoord3").roles()
+    }
+}
